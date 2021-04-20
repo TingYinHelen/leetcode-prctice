1112366b47ebca20df4c3ffff3def77430adbfc2
helen <yinhelen.hlj@qq.com>
2021-04-20 14:38:43 +0800

    rotate

--- rotate/index.js
+++ rotate/index.js
@@ -3,13 +3,34 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-  let _nums = nums.splice(-k - 1, k + 1);
+// var rotate = function(nums, k) {
+//   let _nums = nums.splice(-k - 1, k + 1);
   
-  // console.log('_nums----', _nums);
-  // console.log('nums----', nums);
-  // console.log('result----', [..._nums, ...nums]);
-  return _nums.concat(nums);
-};
+//   // console.log('_nums----', _nums);
+//   // console.log('nums----', nums);
+//   // console.log('result----', [..._nums, ...nums]);
+//   return _nums.concat(nums);
+// };
 
-console.log('result:', rotate([1,2,3,4,5,6,7], 3));
+// console.log('result:', rotate([1,2,3,4,5,6,7], 3));
+let reverse = function (nums, start, end) {
+  while(start < end) {
+    [nums[start++], nums[end--]] = [nums[end], nums[start]]
+  }
+}
+var rotate = function(nums, k) {
+  k = k % nums.length;
+  console.log('k: ', k);
+  reverse(nums, 0, nums.length - 1)
+  reverse(nums, 0, k-1)
+  reverse(nums, k, nums.length - 1)
+  console.log(nums);
+}
+rotate([-1], 2)
+
+
+// for (let i = 0; i < k; i++) {
+//   let a = nums.pop()
+//   nums.unshift(a)
+// }
+// return nums
---
+++ rotate/index.md
@@ -0,0 +1,5 @@
+temp
+temp = nums[0]
+位置0的元素会放置到(0+k)%n的位置
+x = (0+k)%n
+交换temp 和 nums[x]，完成位置x的更新
